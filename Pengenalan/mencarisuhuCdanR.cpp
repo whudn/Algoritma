2cebdc24d006502c974ae74031fdbbbf0f05b181
Wahyu <43489658+whudn@users.noreply.github.com>
2024-03-11 00:02:57 +0700

    improve

--- Pengenalan/mencarisuhuCdanR.cpp
+++ Pengenalan/mencarisuhuCdanR.cpp
@@ -1,19 +1,16 @@
-//Mencari suhu C dan R
 #include <stdio.h>
-#include <conio.h>
 
-main()
-{
-		int r,f,c;
-		printf("Silahkan masukan suhu dalam Fahrenheit:");
-		scanf("%d",&f);
-		
-		c=(f-32)*5/9;
-		r=(f-32)*4/9;
-		
-		printf("Hasil derajat Celciusnya adalah: %d\n",c);
-		printf("Hasil derajat Reamurnya adalah: %d\n",r);
-		
-		getch();
-		
+int main() {
+    float f, c, r;
+
+    printf("Silahkan masukan suhu dalam Fahrenheit: ");
+    scanf("%f", &f);
+
+    c = (f - 32) * 5.0 / 9.0;
+    r = (f - 32) * 4.0 / 9.0;
+
+    printf("Hasil derajat Celciusnya adalah: %.2f\n", c);
+    printf("Hasil derajat Reamurnya adalah: %.2f\n", r);
+
+    return 0;
 }
